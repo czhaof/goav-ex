c6751db4e1438e26b6ca8c0f931cf3c4f8e5e6e3
czf <13960267171@163.com>
2022-01-08 17:09:36 +0800

    fork仓更正路径

--- avformat/stream_struct.go
+++ avformat/stream_struct.go
@@ -9,8 +9,8 @@ import "C"
 import (
 	"unsafe"
 
-	"github.com/giorgisio/goav/avcodec"
-	"github.com/giorgisio/goav/avutil"
+	"github.com/czhaof/goav-ex/avcodec"
+	"github.com/czhaof/goav-ex/avutil"
 )
 
 func (avs *Stream) CodecParameters() *avcodec.AvCodecParameters {
